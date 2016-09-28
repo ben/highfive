bc49b500d63c4033c7ebf34bbe35c1ba494089f7
Ben Straub <ben@straub.cc>
2016-09-27 19:02:04 -0500

    Require login for admin views

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,15 @@
 class AdminController < ApplicationController
+  before_action :requires_login, except: [:login]
+
   def index
   end
 
   def login
+  end
+
+  private
+
+  def requires_login
+    redirect_to '/admin/login' unless false
   end
 end
