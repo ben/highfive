ae33a65f6001d4636bdd1d4b0d361ac2e9310531
Ben Straub <ben@straub.cc>
2016-09-27 19:06:50 -0500

    First test for login redirection

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class AdminControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'redirects to /login if not logged in' do
+    get '/admin/'
+    assert_redirected_to '/admin/login'
+  end
 end
