48421155b8985f27edc343102d908805562e4c0d
Ben Straub <ben@straub.cc>
2016-09-29 21:03:31 -0500

    Fix redis config

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Highfive
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.cache_store = :redis_store, "#{ENV['REDISTOGO_URL']}/0/cache", expires_in: 90.minutes
+    config.cache_store = :redis_store, "#{ENV['REDISTOGO_URL']}/0/cache", { expires_in: 90.minutes }
 
   end
 end
