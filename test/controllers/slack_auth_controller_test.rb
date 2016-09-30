247630548aa3f801f738e346549e7cd01d35a47c
Ben Straub <ben@straub.cc>
2016-09-29 21:31:58 -0500

    Bad test

--- test/controllers/slack_auth_controller_test.rb
+++ test/controllers/slack_auth_controller_test.rb
@@ -1,6 +1,16 @@
 require 'test_helper'
 
 class SlackAuthControllerTest < ActionDispatch::IntegrationTest
+  test 'records login info in session' do
+    get '/slack_auth', {
+      user: { name: 'user name', id: 'userid' },
+      team: { id: 'teamid' }
+    }
+
+    assert_equal session[:user_name], 'user name'
+    assert_equal session[:user_id], 'userid'
+    assert_equal session[:team_id], 'teamid'
+  end
   # test "the truth" do
   #   assert true
   # end
