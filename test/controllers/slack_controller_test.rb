ddf228d3e7ab22ec7955d41d52f67b02aa052b0c
Ben Straub <ben@straub.cc>
2016-09-30 10:52:20 -0700

    Test for command controller

--- test/controllers/slack_controller_test.rb
+++ test/controllers/slack_controller_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class SlackControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  def body
+    JSON.parse response.body
+  end
+
+  test 'ssl check' do
+    post '/slack/command', params: { ssl_check: true }
+    assert_response :success
+  end
+
+  test 'slack token check' do
+    post '/slack/command', params: {
+      token: 'not a match'
+    }
+    assert_response :unauthorized
+  end
+
+  test 'happy path for command' do
+    post '/slack/command', params: {
+      token: ENV['SLACK_VERIFICATION_TOKEN'],
+      user_id: 'userid',
+      text: '@foo for bar'
+    }
+    assert_equal 'in_channel', body['response_type']
+    assert_includes body['text'], '<!channel>'
+    assert_includes body['text'], '<@foo>'
+  end
+
+  test 'badly formatted command' do
+    post '/slack/command', params: {
+      token: ENV['SLACK_VERIFICATION_TOKEN'],
+      user_id: 'userid',
+      text: '@foo for bar'
+    }
+    assert_nil body['response_type']
+    assert_includes body['text'], '`/highfive @user for (reason)`'
+  end
 end
