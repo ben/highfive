5d139a034d9130bbc53830de997979cc029b0b5a
Ben Straub <ben@straub.cc>
2016-10-02 08:24:53 -0700

    Refactor slack_client into a service module

---
+++ app/services/slack_client.rb
@@ -0,0 +1,6 @@
+module SlackClient
+  def slack_client(team_id: nil)
+    team = SlackTeam.find_by_team_id(team_id || session[:team_id] || params[:team_id])
+    Slack::Web::Client.new(token: team.access_token)
+  end
+end
