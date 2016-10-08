0ee70efd7e0ec730f05ad23323df7b91a9fa743d
Ben Straub <ben@straub.cc>
2016-10-08 15:57:53 -0700

    Always be securin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include SlackClient
 
   protect_from_forgery with: :exception
+  before_action :redirect_to_https
 
   helper_method :add_app_url, :slack_login_url
 
@@ -18,6 +19,12 @@ class ApplicationController < ActionController::Base
     "?client_id=#{ENV['SLACK_CLIENT_ID']}" \
     "&redirect_uri=#{ENV['HOSTNAME']}#{url_for '/slack_auth'}" \
     '&scope=users:read'
+  end
+
+  private
+
+  def redirect_to_https
+    redirect_to protocol: 'https://' unless (request.ssl? || request.local?)
   end
 
 end
