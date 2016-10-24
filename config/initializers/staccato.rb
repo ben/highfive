ee27cc23d31baa726ebd701866984df4a83de435
Ben Straub <ben@straub.cc>
2016-10-23 21:07:14 -0700

    Always init

--- config/initializers/staccato.rb
+++ config/initializers/staccato.rb
@@ -1,4 +1,1 @@
-if ENV['GOOGLE_ANALYTICS_ID'].present?
-  puts "Initialing Google analytics with ID '#{ENV['GOOGLE_ANALYTICS_ID']}'"
-  GoogleTracker = Staccato.tracker(ENV['GOOGLE_ANALYTICS_ID'], nil, ssl: true)
-end
+GoogleTracker = Staccato.tracker(ENV['GOOGLE_ANALYTICS_ID'], nil, ssl: true)
