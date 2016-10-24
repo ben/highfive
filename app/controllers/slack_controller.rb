34ab8182df495bed7aa4a951cd6e861c62c1a395
Ben Straub <ben@straub.cc>
2016-10-23 21:07:36 -0700

    Explicit commit for highfive service

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -19,6 +19,7 @@ class SlackController < ApplicationController
     # team = SlackTeam.find_by_team_id params[:team_id]
 
     highfive = HighfiveService::Highfive.new slack_team, params[:user_id], params[:target_user_id], params[:reason], params[:amount]
+    highfive.commit!
     render json: highfive.message
   end
 
