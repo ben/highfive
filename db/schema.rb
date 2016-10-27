e8fbe8592d88f7fdc49ddc4dfb0033dad75fd2e7
Ben Straub <ben@straub.cc>
2016-10-26 19:16:16 -0700

    Award amounts are floats

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161026011345) do
+ActiveRecord::Schema.define(version: 20161027021203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20161026011345) do
     t.string   "to"
     t.string   "reason"
     t.string   "currency"
-    t.integer  "amount"
+    t.float    "amount"
     t.string   "card_code"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
