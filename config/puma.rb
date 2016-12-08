7fe392943052f85b999ee9c9a5ccb87e4d3e7bd0
Ben Straub <ben@straub.cc>
2016-12-08 09:57:32 -0800

    Production puma config

--- config/puma.rb
+++ config/puma.rb
@@ -45,3 +45,22 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if ENV.fetch("RAILS_ENV") == 'production'
+  app_dir = File.expand_path('../..', __FILE__)
+  shared_dir = "#{app_dir}/shared"
+
+  workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+  bind "unix://#{shared_dir}/sockets/puma.sock"
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+  activate_control_app
+
+  on_worker_boot do
+    require 'active_record'
+    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
+
+  preload_app!
+end
