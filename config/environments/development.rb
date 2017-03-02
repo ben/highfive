58007c8b984fca21619f88c19646ccf54da58057
Ben Straub <ben@straub.cc>
2017-03-01 19:26:13 -0800

    Stdout logging fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,5 +54,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.logger = Logger.new(STDOUT)
+  config.logger = ActiveSupport::Logger.new(STDOUT)
 end
