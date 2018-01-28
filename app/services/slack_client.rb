82d3ef01a90d67778ef7130daa68b5b9956209f0
Ben Straub <ben@straub.cc>
2018-01-28 15:24:27 -0800

    Fix tests

--- app/services/slack_client.rb
+++ app/services/slack_client.rb
@@ -3,7 +3,7 @@ module SlackClient
 
   def slack_client(team_id: nil)
     @team_id = team_id || slack_team_id || session[:team_id] || params[:team_id] || @json['team']['id']
-    team = SlackTeam.find_by_team_id(@team_id)
+    team = SlackTeam.find_by(team_id: @team_id)
     Slack::Web::Client.new(token: team.access_token)
   end
 
