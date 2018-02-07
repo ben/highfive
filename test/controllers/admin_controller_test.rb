792eca4003571319a6fa10f4e6336247e04d65fb
Ben Straub <ben@straub.cc>
2018-02-06 19:26:18 -0800

    Test admin controller

--- test/controllers/admin_controller_test.rb
+++ test/controllers/admin_controller_test.rb
@@ -5,4 +5,38 @@ class AdminControllerTest < ActionDispatch::IntegrationTest
     get '/admin/'
     assert_response :redirect
   end
+
+  test 'highfives.csv sends a file' do
+    stub_slack_client
+    get '/slack_auth', params: {code: '2345'}
+    get '/admin/highfives.csv'
+    assert_response :success
+    @response.headers['Content-Disposition'].must_match /attachment.*csv/
+    @response.body.must_match /date,from,to,reason,amount/
+  end
+
+  test 'highfives.xlsx sends a file' do
+    stub_slack_client
+    get '/slack_auth', params: {code: '2345'}
+    get '/admin/highfives.xlsx'
+    assert_response :success
+    @response.headers['Content-Disposition'].must_match /attachment.*xlsx/
+  end
+
+  test 'fundings.csv sends a file' do
+    stub_slack_client
+    get '/slack_auth', params: {code: '2345'}
+    get '/admin/fundings.csv'
+    assert_response :success
+    @response.headers['Content-Disposition'].must_match /attachment.*csv/
+    @response.body.must_match /date,amount,success,response payload/
+  end
+
+  test 'fundings.xlsx sends a file' do
+    stub_slack_client
+    get '/slack_auth', params: {code: '2345'}
+    get '/admin/fundings.xlsx'
+    assert_response :success
+    @response.headers['Content-Disposition'].must_match /attachment.*xlsx/
+  end
 end
